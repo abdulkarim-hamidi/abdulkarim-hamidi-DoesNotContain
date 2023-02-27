3e75cc355a9bfe8b4bd70df0d695aa86050494b2
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-27 21:19:16 +0000

    lab completed

--- src/main/java/DoesNotContain.java
+++ src/main/java/DoesNotContain.java
@@ -1,13 +1,19 @@
 
 public class DoesNotContain {
     /**
-     * The inverse of the contains value challenge: check if the array does *not* contain the value.
-     * You should think critically about the logic that you write within the for loop.
+     * The inverse of the contains value challenge: check if the 
+     array does *not* contain the value.
+     * You should think critically about the logic that you write 
+     within the for loop.
      *
-     * For a trickier problem like this, you should try explaining your thought process of working
-     * through this problem with pencil and paper in plain English before attempting it in code.
-     * For instance: for the contains problem, your code had to check every value to see if the value
-     * matches the target, and if it matched the target, the array was sure to contain the target. Think
+     * For a trickier problem like this, you should try explaining 
+     your thought process of working
+     * through this problem with pencil and paper in plain English 
+     before attempting it in code.
+     * For instance: for the contains problem, your code had to check
+     *  every value to see if the value
+     * matches the target, and if it matched the target, the array was 
+     * sure to contain the target. Think
      * of a similar process here.
      *
      * @param arr the array we will be manipulating.
@@ -15,6 +21,14 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        return false;
+        for(int i = 0; i < arr.length; i++){
+            // boolean b = false;
+            if(arr[i] == target){
+            return false;
+        }
+        
+    }
+    return true;
+       
     }
 }
